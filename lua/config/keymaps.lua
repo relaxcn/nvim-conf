a5a666ec52522fc6e5b655cfb17178b3995d0efb
RelaxCN <64476349+relaxcn@users.noreply.github.com>
2024-09-30 02:42:02 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>o", "o<Esc>", { desc = "Insert line below" })
+vim.keymap.set("n", "<leader>O", "O<Esc>", { desc = "Insert line above" })
